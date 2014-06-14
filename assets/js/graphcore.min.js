559de7f3a59a4f3c90450a67a00cedaab50ccd5c
tmahmood <mahmood.tarin@gmail.com>
2014-06-15 00:16:25 +0600

    Simpleline graph is simplified

--- assets/js/graphcore.min.js
+++ assets/js/graphcore.min.js
@@ -126,89 +126,10 @@ GraphCore.prototype.GroupedMultiBar= function(_data, me) {
 	});
 };
 
-GraphCore.prototype.SimpleLine = function(_data, me) {
-	var data = _data[0];
-	if (data.convertTo != undefined) {
-		console.log(data.values);
-		for (var ik in data.values) {
-			console.log(ik);
-			var d = data.values[ik].values;
-			for (var v in d) {
-				d[v][0] = core.exectueStringAsFunction(data.convertTo, window, d[v]);
-			}
-		}
-	}
-	nv.addGraph(function() {
-		var chart = nv.models.lineWithFocusChart()
-			.x(function(d) {
-				console.log(d[0]);
-				return d[0];
-			})
-			.y(function(d) {
-				console.log(d[1]);
-				return d[1] / 100;
-			})
-			.transitionDuration(750)
-			.showLegend(true);
-
-		if (_data[0].colors != undefined) {
-			chart.color(_data[0].colors)
-		}
-
-		d3.select(me.container + ' svg ').text("")
-		  .datum(data)
-		  .transition().duration(500)
-		  .call(chart);
-
-		nv.utils.windowResize(chart.update);
-		return chart;
-	});
-}
-
-
-GraphCore.prototype.SimpleLine1= function(_data, me) {
+GraphCore.prototype.SimpleLine= function(_data, me) {
 	var data = _data[0].values;
     nv.addGraph(function() {
-        var chart = nv.models.lineWithFocusChart()
-        .x(function(d) { return d[0] })
-        .y(function(d) { return d[1] / 100 })
-        .transitionDuration(750)
-        .showLegend(true);
-
-		if (_data[0].colors != undefined) {
-			chart.color(_data[0].colors)
-		}
-
-    chart.xAxis.tickValues([1354286240000,1364740640000,1367332640000,1375281440000,1383230240000,1385822240000,1391179040000, 1399053600000]) //note: using epoch time = milliseconds since 1/1/1970
-        .tickFormat(function(d) { return d3.time.format('%m/%y')(new Date(d))} )
-        .axisLabel('Month / Year');
-
-    chart.x2Axis.tickValues([1354286240000,1364740640000,1367332640000,1375281440000,1383230240000,1385822240000,1391179040000, 1399053600000])
-        .tickFormat(function(d) {return d3.time.format('%m/%y')(new Date(d))});
-
-    chart.yAxis
-        .tickFormat(d3.format('%x'))
-        .axisLabel('Percentage');
-
-    chart.y2Axis
-        .tickFormat(d3.format('%x'))
-        .axisLabel('Percentage');
-
-  d3.select(me.container + ' svg ').text("")
-      .datum(data)
-      .transition().duration(500)
-      .call(chart);
-
-  nv.utils.windowResize(chart.update);
-
-  return chart;
-});
-}
-
-GraphCore.prototype.SimpleLine2= function(_data, me) {
-	var data = _data[0].values;
-	nv.addGraph(function() {
-	  var chart = nv.models.lineWithFocusChart()
+		var chart = nv.models.lineWithFocusChart()
 			.x(function(d) { return d[0] })
 			.y(function(d) { return d[1] / 100 })
 			.transitionDuration(750)
@@ -218,19 +139,14 @@ GraphCore.prototype.SimpleLine2= function(_data, me) {
 			chart.color(_data[0].colors)
 		}
 
-		chart.xAxis.tickValues([1354286240000,1364740640000,1367332640000,1375281440000,1383230240000,1385822240000,1391179040000, 1399053600000]) //note: using epoch time = milliseconds since 1/1/1970
-			.tickFormat(function(d) {
-				return d3.time.format('%m/%y')(new Date(d))
-			  })
-			.axisLabel('Month / Year')
-		;
+		chart.xAxis.tickValues(_data['tickvalues']) //note: using epoch time = milliseconds since 1/1/1970
+			.tickFormat(function(d) { return d3.time.format('%m/%y')(new Date(d))} )
+			.axisLabel('Month / Year');
 
-		chart.x2Axis.tickValues([1354286240000,1364740640000,1367332640000,1375281440000,1383230240000,1385822240000,1391179040000, 1399053600000]) //note: using epoch time = milliseconds since 1/1/1970
-			.tickFormat(function(d) {
-				return d3.time.format('%m/%y')(new Date(d))
-		  });
+		chart.x2Axis.tickValues(_data['tickvalues'])
+			.tickFormat(function(d) {return d3.time.format('%m/%y')(new Date(d))});
 
-		chart.yAxis     //Chart y-axis settings
+		chart.yAxis
 			.tickFormat(d3.format('%x'))
 			.axisLabel('Percentage');
 
@@ -238,16 +154,14 @@ GraphCore.prototype.SimpleLine2= function(_data, me) {
 			.tickFormat(d3.format('%x'))
 			.axisLabel('Percentage');
 
+		d3.select(me.container + ' svg ').text("")
+			.datum(data)
+			.transition().duration(500)
+			.call(chart);
 
-	  d3.select(me.container + ' svg ').text("")
-		  .datum(data)
-		  .transition().duration(500)
-		  .call(chart);
-
-	  nv.utils.windowResize(chart.update);
-
-	  return chart;
-	})
+		nv.utils.windowResize(chart.update);
+		return chart;
+	});
 }
 
 
